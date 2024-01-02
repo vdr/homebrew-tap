b83cb050a770f8ab11f4ec2086fa5600a200e47d
Vincent De Rijcke <vincent.derijcke@bjss.com>
2024-01-02 16:25:35 +0000

    Upgrade openssh-service to 1.6.0

--- Formula/openssh-service.rb
+++ Formula/openssh-service.rb
@@ -1,19 +1,21 @@
 class OpensshService < Formula
   desc "A MacOS Launchd Service for Brew OpenSSH"
   homepage "https://github.com/vdr/openssh-service/"
-  url "https://github.com/vdr/openssh-service/archive/v1.2.3.tar.gz"
-  sha256 "8f48c40d9fcc5822ee12a893df74dc145cd5836ead4e7b4146c54f07d8f56909"
+  url "https://github.com/vdr/openssh-service/archive/v1.6.0.tar.gz"
+  sha256 "658e10c5eec54302b1d06b73f6385759b9f63907979e914bbbeebad277be75ec"
   
   depends_on "openssh"
   depends_on "vdr/tap/ssh-askpass"
   
   def install
     bin.install name.to_s
+    inreplace "openssh-service.conf", "/tmp/openssh-service.socket", "#{HOMEBREW_PREFIX}/var/run/openssh-service.sock"
+    (etc/"ssh_config.d").install "openssh-service.conf"
   end
   
   service do
     run [opt_bin/"openssh-service", "launchd"]
-    environment_variables PATH: std_service_path_env
+    environment_variables PATH: std_service_path_env, OPENSSH_SERVICE_SOCK: var/"run/openssh-service.sock"
     log_path var/"log/openssh-service.log"
     error_log_path var/"log/openssh-service.log"
   end
@@ -24,7 +26,9 @@ class OpensshService < Formula
 
   def caveats
     <<~EOS
-      Run the following command in your .bashrc or .zshrc
+      [Recommended] Add the following line to your .ssh/config
+        Include #{etc}/ssh_config.d/openssh-service.conf
+      Alternatively, run the following command in your .bashrc or .zshrc
         eval $(#{name} session)
     EOS
   end
--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Variety of homebrew formulae.
 
 Or `brew tap vdr/tap` and then `brew install <formula>`.
 
-## Fido2 SSH Support
+## Brew OpenSSH Launchd Service (support FIDO2!)
 
-`brew install vdr/tap/vdr-fido2-ssh-support`
+`brew install vdr/tap/openssh-service`
 
 For Apple Silicon Macs, Sonoma (14+)
 
@@ -20,6 +20,8 @@ Unfortunately [it is not working out of the box](https://github.com/Yubico/libfi
 
 Instead, enable the vanilla OpenSSH agent, with necessary tooling to make it work.
 
+See [openssh-service](https://github.com/vdr/openssh-service/)
+
 ## ssh-askpass
 
 `brew install vdr/tap/ssh-askpass`
@@ -28,3 +30,4 @@ Simple copy of [ssh-askpass](https://github.com/theseal/ssh-askpass),
 but without caveat about the service and some Apple Silicon fixes.
 (equivalent of the service will be done in `openssh-fido-agent-install`)
 
+See [ssh-askpass](https://github.com/vdr/ssh-askpass/)
