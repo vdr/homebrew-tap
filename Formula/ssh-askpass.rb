d91680f0bddf25958f90805b048484115803a0e7
Vincent De Rijcke <vincent.derijcke@bjss.com>
2024-01-02 17:45:21 +0000

    Upgrade ssh-askpass to 2.1.0

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -1,8 +1,8 @@
 class SshAskpass < Formula
   desc "Passphrase dialog for use with OpenSSH in macOS"
   homepage "https://github.com/vdr/ssh-askpass/"
-  url "https://github.com/vdr/ssh-askpass/archive/v2.0.0.tar.gz"
-  sha256 "d994d2dc594766b186a30e247f1d0402b89bbaac86f95585bd2dfdf9afcc61d5"
+  url "https://github.com/vdr/ssh-askpass/archive/v2.1.0.tar.gz"
+  sha256 "8138eb0aa9b1abe91ca9587ebe7cc0e3e84721bc05f106b7c8ac8a038a3701f4"
 
   def install
     bin.install name.to_s
