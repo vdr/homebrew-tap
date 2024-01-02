9dee55e2541b783f90214fdee357499554989c47
Vincent De Rijcke <vincent.derijcke@bjss.com>
2024-01-02 01:41:09 +0000

    Upgrade openssh-service

--- Formula/openssh-service.rb
+++ Formula/openssh-service.rb
@@ -1,23 +1,31 @@
 class OpensshService < Formula
-  desc "A OSX Service for brew OpenSSH"
+  desc "A MacOS Launchd Service for Brew OpenSSH"
   homepage "https://github.com/vdr/openssh-service/"
-  url "https://github.com/vdr/openssh-service/archive/v1.1.0.tar.gz"
-  sha256 "c13c5e86a59896aba7a74731e6d34362abf4bd3fa8bf9d93cf30a4ad7cd802e9"
+  url "https://github.com/vdr/openssh-service/archive/v1.2.3.tar.gz"
+  sha256 "8f48c40d9fcc5822ee12a893df74dc145cd5836ead4e7b4146c54f07d8f56909"
   
   depends_on "openssh"
   depends_on "vdr/tap/ssh-askpass"
   
-  service do
-    run [Formula["openssh"].opt_bin/"/opt/homebrew/bin/ssh-agent", "-D", "-a", "/tmp/ssh-agent.socket"]
-    environment_variables "SSH_ASKPASS"=>Formula["vdr/tap/ssh-askpass"].opt_bin/"ssh-askpass", "SSH_ASKPASS_REQUIRE"=>"force"
-    keep_alive true
-  end
-
   def install
     bin.install name.to_s
   end
   
+  service do
+    run [opt_bin/"openssh-service", "launchd"]
+    environment_variables PATH: std_service_path_env
+    log_path var/"log/openssh-service.log"
+    error_log_path var/"log/openssh-service.log"
+  end
+  
   test do
     system "true"
+  end
+
+  def caveats
+    <<~EOS
+      Run the following command in your .bashrc or .zshrc
+        eval $(#{name} session)
+    EOS
   end
 end
